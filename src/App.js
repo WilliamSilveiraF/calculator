81c52dbdb031072dd9e67982fffd3ceee765a193
William Silveira <williamsilveiraf@hotmail.com>
2022-01-07 13:37:24 -0300

    functions

--- src/App.js
+++ src/App.js
@@ -10,8 +10,24 @@ import keys from "./data";
 import { Button } from '@material-ui/core'
 
 function App() {
-  const [result, setResult] = useState('8 + 2')
-
+  const [result, setResult] = useState('')
+  
+  const handleClick = (value, id) => {
+    if (value) {
+      setResult(result.concat(value))
+    } else if (id === 'RESET') {
+      setResult('')
+    } else if (id === 'DEL') {
+      setResult(result.slice(0, -1))
+    } else if (id === 'RESULT') {
+      try {
+        setResult(eval(result).toString())
+      } catch (err) {
+        setResult('Error')
+      }
+    }
+  }
+  
   return (
     <Main>
       <Container>
@@ -19,18 +35,18 @@ function App() {
 
         </Screen>
         <Keyboard>
-          { keys.map((key, index) => {
-            const { id, background, color, pad } = key
+          { keys.map((key) => {
+            const { id, background, color, pad, value } = key
             return (
               <Button
                 variant="contained"
                 id={ id }
+                onClick={() => handleClick(value, id)}
                 style={{
                   background: `${background ? background : '#262834'}`,
                   fontSize: '1.1rem',
                   fontWeight: '500',
                   color: `${color ? color : 'white'}`,
-                  
                 }}
               >
                 { pad }
